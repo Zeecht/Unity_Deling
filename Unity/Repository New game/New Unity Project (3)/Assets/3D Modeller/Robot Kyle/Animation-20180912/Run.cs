c0cbd3c6cdb7a8688bd3ca010e715a8b5704b738
Zeech <Morten_hansen51@yahoo.dk>
2018-09-12 17:56:55 +0200

    Fix

---
+++ Unity/Repository New game/New Unity Project (3)/Assets/3D Modeller/Robot Kyle/Animation-20180912/Run.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Run : MonoBehaviour
+{
+    public Animator animator;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Speed", v);
+        animator.SetFloat("Direction", h);
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("Wave");
+        }
+
+    }
+}
---
+++ Unity/Repository New game/New Unity Project (3)/Assets/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float turnSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Vector3 newRotation = transform.rotation.eulerAngles;
+
+        //newRotation.z -= Input.GetAxis("Horizontal") * turnSpeed;
+
+        //transform.rotation = Quaternion.Euler(newRotation);
+
+
+
+        //Vector3 direction = Vector3.zero;
+
+        //direction.x = Input.GetAxis("Horizontal");
+
+        //Vector3 movement = (direction * speed) * Time.deltaTime;
+
+        //direction.y = Input.GetAxis("Vertical");
+
+        //movement = (direction * speed) * Time.deltaTime;
+
+        //transform.Translate(movement);
+
+        //if (Input.GetKey(KeyCode.W))
+        //{
+
+        //    transform.Translate((Vector2.up * speed) * Time.deltaTime);
+
+        //}
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+        {
+
+            //transform.Translate((Vector2.left * speed) * Time.deltaTime);
+            Vector3 newrotation = transform.rotation.eulerAngles;
+            newrotation.z -= Input.GetAxis("Horizontal") * turnSpeed;
+            transform.rotation = Quaternion.Euler(newrotation);
+
+        }
+
+    }
+}
